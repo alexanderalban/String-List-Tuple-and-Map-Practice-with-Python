4d505b508630a25545ab0e49cad3d21a4774bce0
alexanderalban <albanalexander@gmail.com>
2022-06-01 14:52:00 -0500

    list practice added

--- StringListTupleMapPython.py
+++ StringListTupleMapPython.py
@@ -38,3 +38,70 @@ print(joke_text % bodypart2)
 # Using more than one placeholder in a string
 nums = 'What did the number %s say to the number %s? Nice belt!'
 print(nums % (0, 8))
+
+# Multiplying strings
+spaces = ' ' * 25
+print('%s 12 Burts Wynd' % spaces)
+print('%s Twinkebottom Heath' % spaces)
+print('%s West Snoring' % spaces)
+print()
+print()
+print('To Whom It May Concern')
+print()
+print('Hello.')
+print()
+print('XOXO')
+print("Anonymous")
+
+print(1000 * 'boop')
+
+# Lists!
+
+wizard_list = ['spider legs', 'toe of frog', 'eye of newt',
+               'bat wing', 'slug butter', 'snake dandruff']
+print(wizard_list)
+
+# Single out list item
+print(wizard_list[2])
+
+# Change item in list
+wizard_list[2] = "snail tongue"
+print(wizard_list)
+
+# Subset of items
+print(wizard_list[2:5])
+
+# List within a list
+numbers = [1, 2, 3, 4]
+strings = ['I', 'kicked', 'my', 'toe', 'and', 'it', 'is', 'sore']
+myList = [numbers, strings]
+print(myList)
+
+# Add items to a list
+wizard_list.append('bear burp')
+print(wizard_list)
+
+wizard_list.append('mandrake')
+wizard_list.append('hemlock')
+wizard_list.append('swamp gas')
+print(wizard_list)
+
+# Remove items from a list
+del wizard_list[5]
+print(wizard_list)
+
+del wizard_list[8]
+del wizard_list[7]
+del wizard_list[6]
+print(wizard_list)
+
+# Joining Lists
+list1 = [1, 2, 3, 4]
+list2 = ['I', 'tripped', 'over', 'and', 'hit', 'the', 'floor']
+print(list1 + list2)
+
+list3 = list1 + list2
+print(list3)
+
+list4 = [1, 2]
+print(list4 * 5)
